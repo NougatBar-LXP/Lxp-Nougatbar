1f9f85960dab5d05b41217e0a9b133edccc29d63
okjaeho <dhrwogh@gmail.com>
2026-05-20 14:19:24 +0900

    init commit

---
+++ src/main/java/com/nougatbar/lxp/LxpNougatbarApplication.java
@@ -0,0 +1,13 @@
+package com.nougatbar.lxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpNougatbarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxpNougatbarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nougatbar/lxp/LxpNougatbarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nougatbar.lxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxpNougatbarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
