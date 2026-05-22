6ebfdf5e444107659b1e459ad27366d3332d38f4
johypark97 <johypark97@gmail.com>
2026-05-22 15:17:28 +0900

    docs: Tag Entity와 Course 더미 데이터의 주석 오타 수정

--- src/main/java/com/nougatbar/lxp/course/entity/Tag.java
+++ src/main/java/com/nougatbar/lxp/course/entity/Tag.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Table;
 import java.util.Objects;
 
 /**
- * 태그(Rag) 엔티티 클래스
+ * 태그(Tag) 엔티티 클래스
  * <p>
  * <b>Note:</b> 강좌 데이터의 추가/삭제는 구현하지 않고 하드 코딩된 데이터를 이용하는 프로젝트이므로 데이터
  * 변경에 관련된 메소드와 설정들은 구현하지 않았습니다.
