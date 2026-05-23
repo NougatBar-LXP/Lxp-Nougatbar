dd34d06a225b848df38f092853aac8fdd14a59f5
johypark97 <johypark97@gmail.com>
2026-05-23 16:41:01 +0900

    fix: `CartResponse::from` 의 null 및 입력값 검증 강화

--- src/main/java/com/nougatbar/lxp/cart/dto/response/CartResponse.java
+++ src/main/java/com/nougatbar/lxp/cart/dto/response/CartResponse.java
@@ -2,6 +2,7 @@ package com.nougatbar.lxp.cart.dto.response;
 
 import com.nougatbar.lxp.member.dto.response.MemberDTO;
 import java.net.URI;
+import java.util.Objects;
 import java.util.function.Function;
 
 public record CartResponse(Long courseId,
@@ -11,11 +12,17 @@ public record CartResponse(Long courseId,
                            String description,
                            String thumbnailUrl) {
     public static CartResponse from(CartDTO cartDTO, Function<URI, String> uriConverter) {
+        Objects.requireNonNull(cartDTO);
+        Objects.requireNonNull(uriConverter);
+
+        String thumbnailUrl =
+                cartDTO.thumbnailUri() == null ? null : uriConverter.apply(cartDTO.thumbnailUri());
+
         return new CartResponse(cartDTO.courseId(),
                 cartDTO.member(),
                 cartDTO.title(),
                 cartDTO.price(),
                 cartDTO.description(),
-                uriConverter.apply(cartDTO.thumbnailUri()));
+                thumbnailUrl);
     }
 }
