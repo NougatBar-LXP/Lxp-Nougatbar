f03656f4e3b18ce03e83c82f191845b81d228784
johypark97 <johypark97@gmail.com>
2026-05-25 18:23:04 +0900

    feat: 정적 리소스 필터링 제외

--- src/main/java/com/nougatbar/lxp/config/SecurityConfig.java
+++ src/main/java/com/nougatbar/lxp/config/SecurityConfig.java
@@ -1,21 +1,28 @@
 package com.nougatbar.lxp.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/community-ui/**", "/cart-ui/**", "/carts/**", "/css/**", "/community/**").permitAll()
-                        .anyRequest().authenticated()
-                )
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/",
+                        "/community-ui/**",
+                        "/cart-ui/**",
+                        "/carts/**",
+                        "/community/**").permitAll().anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
