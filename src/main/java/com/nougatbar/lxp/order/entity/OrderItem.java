fa87a7e054e5e977035434e41a6d3e6e76389945
okjaeho <dhrwogh@gmail.com>
2026-05-26 09:21:44 +0900

    fix: orderItem entity 0이상 조건으로 수정

--- src/main/java/com/nougatbar/lxp/order/entity/OrderItem.java
+++ src/main/java/com/nougatbar/lxp/order/entity/OrderItem.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 public record OrderItem(Long courseId, int price) {
 
     public OrderItem {
-        Objects.requireNonNull(courseId, "courseId 필수입니다.");
-        if (price <= 0) {
-            throw new IllegalArgumentException("가격은 양수여야 합니다.");
+        Objects.requireNonNull(courseId, "courseId는 필수입니다.");
+        if (price < 0) {
+            throw new IllegalArgumentException("가격은 0 이상이여야 합니다.");
         }
     }
 }
